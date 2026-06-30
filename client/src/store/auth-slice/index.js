c5ac9032e3c41f08af39ee4a58e623067ba99997
Abhijeet Kumar Shah <ajtkr200@gmail.com>
2026-07-01 00:14:48 +0530

    fix: configure secure cookie attributes dynamically based on environment for authentication flow

--- client/src/store/auth-slice/index.js
+++ client/src/store/auth-slice/index.js
@@ -7,6 +7,7 @@ const initialState = {
   isAuthenticated: false,
   isLoading: true,
   user: null,
+  token: null
 };
 
 export const registerUser = createAsyncThunk(
@@ -57,6 +58,26 @@ export const logoutUser = createAsyncThunk(
   }
 );
 
+// export const checkAuth = createAsyncThunk(
+//   "/auth/checkauth",
+
+//   async () => {
+//     const response = await axios.get(
+//       `${API}/api/auth/check-auth`,
+//       {
+//         withCredentials: true,
+//         headers: {
+//           "Cache-Control":
+//             "no-store, no-cache, must-revalidate, proxy-revalidate",
+//         },
+//       }
+//     );
+
+//     return response.data;
+//   }
+// );
+
+
 export const checkAuth = createAsyncThunk(
   "/auth/checkauth",
 
@@ -64,8 +85,9 @@ export const checkAuth = createAsyncThunk(
     const response = await axios.get(
       `${API}/api/auth/check-auth`,
       {
-        withCredentials: true,
+
         headers: {
+          Authorization: `Bearer ${token}`,
           "Cache-Control":
             "no-store, no-cache, must-revalidate, proxy-revalidate",
         },
@@ -76,11 +98,13 @@ export const checkAuth = createAsyncThunk(
   }
 );
 
+
+
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUser: (state, action) => {},
+    setUser: (state, action) => { },
   },
   extraReducers: (builder) => {
     builder
@@ -106,11 +130,14 @@ const authSlice = createSlice({
         state.isLoading = false;
         state.user = action.payload.success ? action.payload.user : null;
         state.isAuthenticated = action.payload.success;
+        state.token = action.payload.token
+        sessionStorage.setItem('token', JSON.stringify(action.payload.token))
       })
       .addCase(loginUser.rejected, (state, action) => {
         state.isLoading = false;
         state.user = null;
         state.isAuthenticated = false;
+        state.token = null;
       })
       .addCase(checkAuth.pending, (state) => {
         state.isLoading = true;
